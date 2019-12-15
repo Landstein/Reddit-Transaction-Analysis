824bf9a55a04c3fbfb8c41620d9c4f02f457c7f2
Landstein <eric@landsteinmail.com>
2019-12-15 11:48:05 -0500

    confirmed trade url api call

---
+++ confirm_scape_eda.py
@@ -0,0 +1,31 @@
+import praw
+import re
+import json
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+import pickle
+import time
+from datetime import datetime
+
+reddit = praw.Reddit(client_id = "",
+                    client_secret = "",
+                    user_agent="",
+                    username = "",
+                    password = "")
+
+#72 months
+def confirm_urls():
+    url_list = []
+    # sets subreddit to hardwareswap to specifically search r/hardwareswap
+    subreddit = reddit.subreddit('hardwareswap')
+        # appends the url for each search result to url_list by using the argument .url
+    for i in subreddit.search("confirmed trade thread", limit=72):
+        url_list.append(i.url)
+    # reverse the list so the URL's are from oldest to newest
+    url_list = url_list[::-1]
+    return url_list
+
+
+#example call
+url_list = confirm_urls()
